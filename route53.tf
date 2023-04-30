2a22d132e5997418a6167981bf32bc283a97eeda
adityalakku <adityalakku03@gmail.com>
2023-04-30 15:56:38 +0530

    commit

---
+++ route53.tf
@@ -0,0 +1,19 @@
+data "aws_route53_zone" "hosted_zone" {
+  name = var.web_domain_name
+}
+
+resource "aws_route53_record" "web" {
+  zone_id = data.aws_route53_zone.hosted_zone.zone_id
+  name    = var.web_sub_domain_name
+  type    = "A"
+  ttl     = 300
+  records = aws_instance.web_server.*.public_ip
+}
+
+resource "aws_route53_record" "mail" {
+  zone_id = data.aws_route53_zone.hosted_zone.zone_id
+  name    = var.mail_sub_domain_name
+  type    = "A"
+  ttl     = 300
+  records = aws_instance.mail_server.*.public_ip
+}
