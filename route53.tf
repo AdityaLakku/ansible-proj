04bab676d9f08d1b8f2fc2b32b82cc762c6d6535
AdityaLakku <119667548+AdityaLakku@users.noreply.github.com>
2023-05-03 12:17:06 +0530

    Update route53.tf

--- route53.tf
+++ route53.tf
@@ -2,6 +2,13 @@ data "aws_route53_zone" "hosted_zone" {
   name = var.domain_name
 }
 
+locals {
+  web_server_ip = aws_instance.web_server.*.public_ip
+  mail_server_ip = aws_instance.mail_server.*.public_ip
+
+}
+
+
 resource "aws_route53_record" "web" {
   count   = var.web_want == true ? 1 : 0
   zone_id = data.aws_route53_zone.hosted_zone.zone_id
